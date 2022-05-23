d8f3cec2dabe19856c92f0c6c09926abc370ad66
pedrorof <peedrorof@gmail.com>
2022-05-23 13:52:49 -0300

    Template do admin ( sucesso )

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  namespace :site do
+    get 'welcome/index'
+  end
+  namespace :admins_backoffice do
+    get 'welcome/index'
+  end
+  namespace :users_backoffice do
+    get 'welcome/index'
+  end
+
+  devise_for :admins
+  devise_for :users
+
+  get 'welcome/index'
+  
+  root to: 'site/welcome#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
